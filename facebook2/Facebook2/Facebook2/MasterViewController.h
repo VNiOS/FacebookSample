01a43061f42115932e95319e341caae93ae9f1d1
tran minh nghia <nghiamtran@gmail.com>
2012-10-04 15:40:30 +0700

    create new project facebook2

---
+++ facebook2/Facebook2/Facebook2/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Facebook2
+//
+//  Created by Nghia on 10/4/12.
+//  Copyright (c) 2012 Nghia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ facebook2/Facebook2/Facebook2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Facebook2
+//
+//  Created by Nghia on 10/4/12.
+//  Copyright (c) 2012 Nghia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
