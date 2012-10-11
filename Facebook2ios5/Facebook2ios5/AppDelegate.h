298597b5db052d0456f8e21bfaae5597eadf1fc3
tran minh nghia <nghiamtran@gmail.com>
2012-10-11 17:50:46 +0700

    Add new project in iOS 5 , can select and tag 1 friends

---
+++ Facebook2ios5/Facebook2ios5/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Facebook2ios5
+//
+//  Created by Applehouse on 10/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Facebook2ios5/Facebook2ios5/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Facebook2ios5
+//
+//  Created by Applehouse on 10/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
